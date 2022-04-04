145ac104f16f11118811f6a90b4e366ce1870604
Zineb-Belbhiriya <zineb.belbhiriya012@gmail.com>
2022-04-04 10:33:43 +0000

    dashbord

---
+++ components/admin/dashbord/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import clsx from "clsx";
+import { useRouter } from "next/router";
+
+export const APP_BAR_HEIGHT = "4.5rem";
+
+export default function NavBar() {
+  const { pathname } = useRouter();
+  return (
+    <header className="sticky inset-0">
+      <nav
+        className="bg-green-300 px-4 py-2 flex items-center justify-between"
+        style={{ height: APP_BAR_HEIGHT }}
+      >
+        <Link href="/">
+          <a className={`text-4xl font-bold ${linkCss(pathname === "/")}`}>
+            Home
+          </a>
+        </Link>
+
+        <div className="space-x-4">
+          <Link href="/about">
+            <a className={`text-2xl ${linkCss(pathname === "/about")}`}>
+              About
+            </a>
+          </Link>
+          <Link href="/team">
+            <a className={`text-2xl ${linkCss(pathname.includes("/team"))}`}>
+              Team
+            </a>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+function linkCss(currentPage: boolean) {
+  return clsx(
+    currentPage ? "text-blue-700" : "text-gray-800",
+    "hover:text-blue-500"
+  );
+}
--- pages/admin/dashbord.tsx
+++ pages/admin/dashbord.tsx
@@ -2,6 +2,8 @@ import Sidebar from "@/components/admin/dashbord/sidebar";
 import Navbar from "@/components/admin/dashbord/nav";
 import Cards from "@/components/admin/dashbord/cards";
 import Header from "@/components/admin/dashbord/header";
+// import Srr from "@/components/admin/dashbord/sidebrr";
+
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
@@ -13,6 +15,7 @@ export default function Dashbord() {
       <Header />
 
       <Sidebar />
+      {/* <Srr /> */}
 
       {/* <Cards /> */}
     </div>
